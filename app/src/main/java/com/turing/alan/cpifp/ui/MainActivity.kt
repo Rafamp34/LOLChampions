d1dc564a6635f3d75b9f50eb3331bc1ff498c95b
RAFA <rafaelmoncayop37@gmail.com>
2024-10-02 14:39:12 +0200

    Fixed

--- app/src/main/java/com/turing/alan/cpifp/ui/MainActivity.kt
+++ app/src/main/java/com/turing/alan/cpifp/ui/MainActivity.kt
@@ -5,13 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.turing.alan.cpifp.R
+import com.turing.alan.cpifp.data.InMemoryChampionsRepository
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recycler_view_champions)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val champions = InMemoryChampionsRepository.getInstance().getChampions()
+        val adapter = ChampionAdapter(champions)
+        recyclerView.adapter = adapter
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
